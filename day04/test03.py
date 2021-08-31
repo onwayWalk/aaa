b8f90709d3237461d492efba2125bbfe941b7735
liheqiao <1430094241@qq.com>
2021-08-31 11:35:54 +0800

    第一次更新

--- day04/test03.py
+++ day04/test03.py
@@ -11,10 +11,19 @@ def tongji(x):
         else :
             y[i]=y[i]+1
     return y
-a=[]
-for i in range (30):
-    a.append(random.randint(0,10))
+
 # a=[1,1,1,1,1,2,2,2,2,22,3,3,3,3,33,3,4,4,4,4,4,4,4]
-print(a)
-c=tongji(a)
+# for i in range (30):
+#     a.append(random.randint(0,10))
+def shuzu(a,b,c):
+    shuz=[]
+    for i in range(0,a):
+        shuz.append(random.randint(b,c))
+    return shuz
+a=int(input("请输入列表长度："))
+print('请输入取值区间（空格隔开）：')
+b,c=map(int,input().split())
+
+c=tongji(shuzu(a,b,c))
+
 print(str(c))
