d718c5502c3de6a0c3bd9ee8467b4bd01e3a8db0
liheqiao <1430094241@qq.com>
2021-08-31 11:10:16 +0800

    修改版

--- day04/test03.py
+++ day04/test03.py
@@ -1,5 +1,8 @@
 # 编写一个函数，传入一个列表，并统计每个数字出现的次数。
 # 返回字典数据：{21:3,56:9,10:3}   （阿里一轮笔试题）
+import random
+
+
 def tongji(x):
     y={}
     for i in x:
@@ -8,6 +11,10 @@ def tongji(x):
         else :
             y[i]=y[i]+1
     return y
-a=[1,1,1,1,1,2,2,2,2,22,3,3,3,3,33,3,4,4,4,4,4,4,4]
+a=[]
+for i in range (30):
+    a.append(random.randint(0,10))
+# a=[1,1,1,1,1,2,2,2,2,22,3,3,3,3,33,3,4,4,4,4,4,4,4]
+print(a)
 c=tongji(a)
 print(str(c))
---
+++ day04/test04.py
@@ -0,0 +1,4 @@
+dict1={}
+dict1={1:{2:{3:{4:{5:{6:7}}}}}}
+print(dict1)
+print(dict1[1])
